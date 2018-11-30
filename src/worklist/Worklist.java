1889d3f87e11cfdec1c491c2d6ac1978efe0c0ed
s133206 <s133206@student.dtu.dk>
2018-11-30 12:21:32 +0100

    initialized fifo

--- src/worklist/Worklist.java
+++ src/worklist/Worklist.java
@@ -1,4 +1,17 @@
 package worklist;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Worklist {
+
+    private HashMap<String, ArrayList<AnalysisDomainElement>> A;
+
+    public HashMap<String, ArrayList<AnalysisDomainElement>> fifo (){
+
+        ArrayList<Constraint> worklist = new ArrayList<>();
+        HashMap<String, ArrayList<VariableSet>> infl = new HashMap<>();
+
+        return null;
+    }
 }
