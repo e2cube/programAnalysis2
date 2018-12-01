0ab0d6109352ed5fee4748651451c0033c57e478
s133206 <s133206@student.dtu.dk>
2018-12-01 11:57:17 +0100

    Changed linkedlist to arraylist

--- src/worklist/WorklistAlgorithm.java
+++ src/worklist/WorklistAlgorithm.java
@@ -14,16 +14,50 @@ public class WorklistAlgorithm {
 
     private HashMap<String, ArrayList<AnalysisDomainElement>> A = new HashMap<>();
 
-    public HashMap<String, ArrayList<AnalysisDomainElement>> fifo(LinkedList<Constraint> givenConstraints) {
+    public HashMap<String, ArrayList<AnalysisDomainElement>> fifo(ArrayList<Constraint> givenConstraints) {
 
         Worklist worklist = new Worklist(givenConstraints);
 
-        //HashMap<String, ArrayList<VariableSet>> infl = worklist.CreateInfluence();
-        HashMap<String, ArrayList<AnalysisDomainElement>> A;
+        HashMap<Integer, ArrayList<Integer>> infl = worklist.CreateInfluence();
 
-        Constraint c = givenConstraints.getFirst();
+        HashMap<Integer, Constraint> constraintsLookup = new HashMap<>();
 
-        //A.put(c.getLeftHandSide().getName(), evaluateConstraint(c.getRightHandSide()));
+        HashMap<String, ArrayList<AnalysisDomainElement>> A = new HashMap<>();
+
+        //While the worklist isn't empty.
+        while (!worklist.getConstraints().isEmpty()){
+            Constraint c = worklist.getConstraints().get(0);
+
+            //ArrayList in the Analysis hashmap
+            ArrayList<AnalysisDomainElement> currentList = new ArrayList<>(A.get(c.getLeftHandSide().getName()));
+
+            //ArrayList from evaluating the righthandside of the constraint
+            ArrayList<AnalysisDomainElement> result = c.getRightHandSide().evaluate(A);
+
+            //Union the two arraylists if it already has a list.
+            Boolean isChanged = false;
+            if (currentList != null){
+                for(AnalysisDomainElement el : result){
+                    if (!currentList.contains(el)){
+                        isChanged = true;
+                        currentList.add(el);
+                    }
+                }
+            }
+            else {
+                A.put(c.getLeftHandSide().getName() , currentList);
+            }
+
+            if (isChanged){
+                //Get the ids that should be added
+                ArrayList<Integer> idsToAdd = infl.get(c.getId());
+
+                //Add them to the worklist
+                for (Integer id : idsToAdd){
+                    worklist.getConstraints().add(0,constraintsLookup.get(id));
+                }
+            }
+        }
 
         return null;
     }
