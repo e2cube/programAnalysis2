608cda035cde17b6b1c5ea155126cf6eab28720b
BSalliot <berenice.salliot@gmail.com>
2018-12-02 15:37:00 +0100

    Analysis structure

--- src/Analysis/Analysis.java
+++ src/Analysis/Analysis.java
@@ -1,6 +1,57 @@
 package Analysis;
 
+import com.company.Graph;
+import com.company.OneToOther;
+import com.company.Sequence;
+import worklist.AnalysisDomainElement;
+import worklist.Constraint;
+import worklist.WorklistAlgorithm;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Analysis {
 
+    public enum TypeAnalysis
+    {
+        DANGEROUS,
+        SIGNS;
+    }
 
+    public enum TypeWorklist
+    {
+        FIFO,
+        LIFO,
+        REVERSE;
+    }
+
+    public void Analyse(Sequence abstract_syntax_tree, TypeAnalysis typeAnalysis, TypeWorklist typeWorklist)
+    {
+        OneToOther translation_object = new OneToOther();
+        translation_object.TreeToGraph(abstract_syntax_tree);
+
+        Graph generated_graph = translation_object.graph;
+        AnalysisAlgorithm analysis = new AnalysisAlgorithm(generated_graph);
+
+        ArrayList<Constraint> generated_constraints = new ArrayList<>();
+
+
+        switch (typeAnalysis)
+        {
+            case DANGEROUS: generated_constraints = analysis.DangerousVariablesAnalysis();
+
+        }
+
+        WorklistAlgorithm worklist_algorithm = new WorklistAlgorithm();
+        HashMap<String, ArrayList<AnalysisDomainElement>> A_result = new HashMap<>();
+
+        switch (typeWorklist)
+        {
+            case FIFO : A_result = worklist_algorithm.fifo(generated_constraints);
+            case LIFO : A_result = worklist_algorithm.lifo(generated_constraints);
+            case REVERSE : A_result = worklist_algorithm.ReversePostOrderIteration(generated_constraints);
+        }
+
+        //Then print the A_result
+    }
 }
---
+++ src/Analysis/AnalysisAlgorithm.java
@@ -0,0 +1,28 @@
+package Analysis;
+
+
+import com.company.*;
+import worklist.*;
+
+import java.util.ArrayList;
+
+public class AnalysisAlgorithm {
+
+    private Graph program_graph;
+
+    public AnalysisAlgorithm(Graph program_graph) {
+        this.program_graph = program_graph;
+    }
+
+    public ArrayList<Constraint> DangerousVariablesAnalysis ()
+    {
+        ArrayList<Constraint> generated_constraints = new ArrayList<>();
+
+
+        //TODO
+
+
+        
+        return generated_constraints;
+    }
+}
